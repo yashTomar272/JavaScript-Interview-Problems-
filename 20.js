eea3e4ed5247518a8c251f232bfd77bbff60b9fa
yashTomar272 <tomarji272@gmail.com>
2026-08-29 00:50:37 +0530

    Check Prime Number

---
+++ 20.js
@@ -0,0 +1,10 @@
+// Check Prime Number
+let num=17
+function funName(num){
+if(num<2) return false
+for(let i=2;i<Math.sqrt(num);i++){
+    if(num%i==0) return false
+}
+return true
+}
+console.log(funName(num));
